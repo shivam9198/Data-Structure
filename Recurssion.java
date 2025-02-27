6082569a3259377b5898bd816667ff1cfaf70369
shivam gupta <shivamkashyap9198@gmail.com>
2025-02-28 01:49:55 +0530

    first commit

---
+++ Recurssion.java
@@ -0,0 +1,16 @@
+public class Recurssion {
+
+    public static void Recurssions(int n){
+        int sum =0;
+        if()
+
+    }
+
+    public static void main(String[] args) {
+
+        int n = 10;
+
+        Recurssions(n);
+    }
+    
+}
---
+++ hello.java
@@ -0,0 +1,56 @@
+class Hello {
+
+    public static void BubbleSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+
+            }
+        }
+
+    }
+
+    public static void SelectionSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int smallest = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[smallest]) {
+                    smallest = j;
+                }
+
+            }
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+
+    }
+
+    public static void InsertionSort(int arr[]) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+        // BubbleSort(arr);
+        // SelectionSort(arr);
+        InsertionSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
